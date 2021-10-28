440f10fee232a1854a12df92ef3961c0c8634e86
JacobBas <basjacob17@gmail.com>
2021-10-28 01:25:13 -0700

    minor

---
+++ q_1232/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <assert.h>
+
+bool checkStraightLine(<vector<vector<int>> coordinates) {
+    float num, den;
+}
+
+int main() {
+    <vector<vector<int>> input = {
+        {1, 2}, {2, 3}, {3, 4},
+        {4, 5}, {5, 6}, {6, 7}
+    };
+    bool output = true;
+    std::cout << checkStraightLine(input) << std::endl;
+
+    return 0
+}
